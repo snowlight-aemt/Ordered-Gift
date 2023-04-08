8a451648ef70be2228586876de2366974cdd6b58
snowlight-aemt <it.s.aemt@gmail.com>
2023-04-08 22:30:57 +0900

    LocalStask (AWS SQS) 적용

--- src/main/java/me/snowlight/gift/config/AwsSqsConfig.java
+++ src/main/java/me/snowlight/gift/config/AwsSqsConfig.java
@@ -3,7 +3,7 @@ package me.snowlight.gift.config;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +18,12 @@ public class AwsSqsConfig {
     @Value("${cloud.aws.secret-key}")
     private String awsSecretKey;
 
+    @Value("${cloud.aws.end-point}")
+    private String awsEndpoint;
+
+    @Value("${cloud.aws.region}")
+    private String awsRegion;
+
     @Bean
     public AmazonSQSAsync amazonSQSAsync() {
         AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(
@@ -26,7 +32,7 @@ public class AwsSqsConfig {
 
         return AmazonSQSAsyncClientBuilder
                 .standard()
-                .withRegion(Regions.AP_NORTHEAST_2)
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsEndpoint, awsRegion))
                 .withCredentials(awsCredentialsProvider)
                 .build();
     }
