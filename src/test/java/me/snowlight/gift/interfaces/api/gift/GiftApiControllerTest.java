8a7281d010fde9796de6c81e72962d12e305ccd4
snowlight-aemt <it.s.aemt@gmail.com>
2023-04-15 14:25:57 +0900

    주목 수락까지의 절차에 대해서 테스트 코드 추가
    * awaitility 라이브러리 추가
      * 외부에서 비동기 적으로 들어오는 부분을 커버하기 위해서 사용
    
    * localstack SQS 테스트 환경 구축 (명령어)
    `awslocal --endpoint-url=http://localhost:4566 sqs create-queue --queue-name order-payComplete.fifo --attributes FifoQueue=true`
    `awslocal sqs list-queues`
    
    * testRestTemplate
      * 외부에 요청을 보내기 위해서 사용
    
    * ObjectMother 추가
      * 테스트를 빨리 하기 위해서 필요할 때 마다 지속적으로 추가

--- src/test/java/me/snowlight/gift/interfaces/api/gift/GiftApiControllerTest.java
+++ src/test/java/me/snowlight/gift/interfaces/api/gift/GiftApiControllerTest.java
@@ -8,20 +8,25 @@ import com.navercorp.fixturemonkey.javax.validation.plugin.JavaxValidationPlugin
 import me.snowlight.gift.application.gift.GiftFacade;
 import me.snowlight.gift.common.response.CommonResponse;
 import me.snowlight.gift.domain.gift.Gift;
+import me.snowlight.gift.domain.gift.GiftReader;
 import me.snowlight.gift.domain.gift.order.ItemInfo;
+import me.snowlight.gift.domain.gift.order.OrderDto;
 import me.snowlight.gift.util.TestSpecificLanguage;
 import net.jqwik.api.Arbitraries;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import static org.awaitility.Awaitility.with;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,6 +46,10 @@ class GiftApiControllerTest {
     private ObjectMapper objectMapper;
     @Autowired
     private GiftFacade giftFacade;
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
+    private GiftReader giftReader;
 
     private final static FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
             .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
@@ -56,7 +65,7 @@ class GiftApiControllerTest {
         testSpecificLanguage.requestChangedItemStatusOnSales(itemToken);
 
         ItemInfo.Main itemInfo = testSpecificLanguage.requestRetrieveItem(itemToken);
-        GiftDto.RegisterGift registerGift = getRegisterGift(itemInfo);
+        GiftDto.RegisterGift registerGift = convertToRegisterGift(itemInfo);
 
         CommonResponse<GiftDto.RegisterResponse> response = testSpecificLanguage.requestRegisterGiftOrder(registerGift);
 
@@ -71,26 +80,41 @@ class GiftApiControllerTest {
     }
 
     @DisplayName("선물 주문 수락 ")
-    @RepeatedTest(50)
+    @Test
     void accept() throws Exception {
+        // 파트너 등록
         String partnerToken = testSpecificLanguage.requestRegisterPartner();
+
+        // 상품 등록
         String itemToken = testSpecificLanguage.requestRegisterItem(partnerToken);
         testSpecificLanguage.requestChangedItemStatusOnSales(itemToken);
 
+        // 주문 등록
         ItemInfo.Main itemInfo = testSpecificLanguage.requestRetrieveItem(itemToken);
-        GiftDto.RegisterGift registerGift = getRegisterGift(itemInfo);
-
+        GiftDto.RegisterGift registerGift = convertToRegisterGift(itemInfo);
         CommonResponse<GiftDto.RegisterResponse> response = testSpecificLanguage.requestRegisterGiftOrder(registerGift);
         String giftToken = response.getData().getGiftToken();
         String orderToken = response.getData().getOrderToken();
 
+        // 결제 중 상태 변경
         testSpecificLanguage.requestPaymentProcessing(giftToken);
 
-        giftFacade.completePayment(orderToken);
+        // 결제 진행 (PG 응답 처리)
+        CommonResponse<OrderDto.Main> responseOrderMain = testSpecificLanguage.requestRetrieveOrder(orderToken);
+        testSpecificLanguage.requestPaymentOrder(responseOrderMain.getData().getOrderToken(),
+                                                responseOrderMain.getData().getTotalAmount(),
+                                                responseOrderMain.getData().getPayMethod());
 
+        with().conditionEvaluationListener(condition -> System.out.println())
+                .await()
+                .atMost(1_000, TimeUnit.MILLISECONDS)
+                .pollInterval(Duration.ofMillis(100))
+                .until(() -> giftReader.getGiftByGiftToken(giftToken).getStatus() == Gift.Status.ORDER_COMPLETE);
+
+
+        // Act
         String paymentProcessingURL = "/api/v1/gifts/" + giftToken + "/accept-gift";
         GiftDto.AcceptGiftReq acceptGiftReq = this.fixtureMonkey.giveMeOne(GiftDto.AcceptGiftReq.class);
-
         mvc.perform(post(paymentProcessingURL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(acceptGiftReq)))
@@ -108,7 +132,7 @@ class GiftApiControllerTest {
         testSpecificLanguage.requestChangedItemStatusOnSales(itemToken);
 
         ItemInfo.Main itemInfo = testSpecificLanguage.requestRetrieveItem(itemToken);
-        GiftDto.RegisterGift registerGift = getRegisterGift(itemInfo);
+        GiftDto.RegisterGift registerGift = convertToRegisterGift(itemInfo);
 
         CommonResponse<GiftDto.RegisterResponse> response = testSpecificLanguage.requestRegisterGiftOrder(registerGift);
 
@@ -126,7 +150,7 @@ class GiftApiControllerTest {
 
     }
 
-    private GiftDto.RegisterGift getRegisterGift(ItemInfo.Main itemInfo) {
+    private GiftDto.RegisterGift convertToRegisterGift(ItemInfo.Main itemInfo) {
         GiftDto.RegisterOrderItem orderItem = fixtureMonkey
                 .giveMeBuilder(this.itemInfoMapper.of(itemInfo))
                 .set("orderCount", Arbitraries.integers().between(1, 127))
